fbd81bf6b923aa8d4f9cad62fe5cfe2c96c2f024
Stephen Davidson <stephen.davidson@pearson.com>
2019-10-01 15:27:48 -0500

    Modify chart with static data related to aws budgets

--- src/Chart.js
+++ src/Chart.js
@@ -2,27 +2,34 @@ import React from 'react';
 import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
 import Title from './Title';
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
+function createData(date, spend, budget) {
+  return { date, spend, budget };
 }
 
-const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
+// TODO: hit an endpoint to collect the data.
+var data = [
+  createData('Oct 2018', 1341.69, 1000),
+  createData('Nov 2018', 1115.29, 1000),
+  createData('Dec 2018', 1275.56, 1000),
+  createData('Jan 2019', 357.57, 1000),
+  createData('Feb 2019', 170.19, 1000),
+  createData('Mar 2019', 191.53, 1000),
+  createData('Apr 2019', 412.81, 1000),
+  createData('May 2019', 385.70, 1000),
+  createData('Jun 2019', 399.78, 1000),
+  createData('Jul 2019', 446.48, 1000),
+  createData('Aug 2019', 554.94, 1000),
+  createData('Sep 2019', 776.88, 1000),
+  createData('Oct 2019', undefined, 1000),
 ];
+
+// TODO: hit an endpoint to collect the title
+var title = 'Monthly AWS Budget io-example 000000000000'
 
 export default function Chart() {
   return (
     <React.Fragment>
-      <Title>Today</Title>
+      <Title>{title}</Title>
       <ResponsiveContainer>
         <LineChart
           data={data}
@@ -33,13 +40,14 @@ export default function Chart() {
             left: 24,
           }}
         >
-          <XAxis dataKey="time" />
+          <XAxis dataKey="date" />
           <YAxis>
             <Label angle={270} position="left" style={{ textAnchor: 'middle' }}>
-              Sales ($)
+              Spend ($)
             </Label>
           </YAxis>
-          <Line type="monotone" dataKey="amount" stroke="#556CD6" dot={false} />
+          <Line type="monotone" dataKey="spend" stroke="#556CD6" dot={false} />
+          <Line type="monotone" dataKey="budget" stroke="#CF2727" dot={false} />
         </LineChart>
       </ResponsiveContainer>
     </React.Fragment>
