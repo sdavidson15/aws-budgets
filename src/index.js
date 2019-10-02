32513d5a4ae3ac55d0925a97890115c346f30efb
sdavidson15 <davidson.sc19@gmail.com>
2019-10-01 20:35:54 -0500

    Stub a home page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from "react-dom"
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
-import Dashboard from "./view/budget-page/Budget";
+import Budget from "./view/budget-page/Budget";
+// import Home from "./view/home-page/Home"
 
 ReactDOM.render(
    <ThemeProvider>
       <CssBaseline />
-      <Dashboard />
+      <Budget />
    </ThemeProvider>,
    document.querySelector('#root') 
 )
